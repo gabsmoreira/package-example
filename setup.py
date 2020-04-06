816d84ae6f231b3346fa418cc24914ba6c9a8eed
gabsmoreira <gabrielfm2@al.insper.edu.br>
2020-04-06 18:43:15 -0300

    add requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
-setup(name='dev_aberto_gabs', version='0.1',
-packages=['dev_aberto'] )
+setup(
+    name='dev_aberto_gabs',
+    version='0.1',
+    packages=['dev_aberto'],
+    scripts=['scripts/hello.py'],
+    author='Gabriel Moreira',
+    author_email='gabriel.moreira98@hotmail.com',
+    install_requires=['requests>=2.22.0']
+)
